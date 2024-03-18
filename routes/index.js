9b691234245deda17295af4100eab6e0c2fa62a7
Cjungwo <jwoo516@naver.com>
2024-03-18 20:31:44 +1100

    18/03/2024 - start project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ecommerce WebApplication' });
+  res.render('index', { title: 'Ecommerce' });
+  next()
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { name: 'name'});
 });
 
 module.exports = router;
